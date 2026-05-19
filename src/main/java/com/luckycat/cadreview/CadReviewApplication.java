80f2bd4a70fcc647327451dee7c0c452eb48aefb
DG <dongchaoyu29@gmail.com>
2026-05-19 11:07:54 +0800

    feat: init Spring AI scaffold with dual provider dependencies

---
+++ src/main/java/com/luckycat/cadreview/CadReviewApplication.java
@@ -0,0 +1,11 @@
+package com.luckycat.cadreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadReviewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CadReviewApplication.class, args);
+    }
+}
